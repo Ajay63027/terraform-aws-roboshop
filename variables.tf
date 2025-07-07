e92e55b2afd27ffb4d9807da0c90e2f393bbdcc1
Ajay <ajaylearner01@gmail.com>
2025-07-07 15:09:37 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z004816720LRYIVJ8OHJQ"
+}
+
+variable "zone_name" {
+    default = "ajay6.space"
+}
+
+variable "component" {
+  
+}
+
+variable "rule_priority" {
+  
+}
